7add0e7d7fb9e301a4fc0cc7bf556934e8a84236
Damien Fleminks <damien@fleminks.com>
2020-04-27 20:36:37 +0100

    First commit

---
+++ broker.js
@@ -0,0 +1,11 @@
+/** MQTT Broker  */
+const mosca = require('mosca')
+
+const settings = {port: 3000}
+
+const broker = new mosca.Server(settings);
+
+broker.on('ready', ()=>{
+    console.log('Broker is ready!');
+});
+
